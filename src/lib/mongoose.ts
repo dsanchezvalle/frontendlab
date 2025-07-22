57b95c23b05d065ca38f93e5ae03118128cabccf
David Sanchez Valle <dsanva2004@gmail.com>
2025-07-21 22:58:15 -0300

    feat: update lib/mongoose.ts to switch prod and dev MongoDB URIs

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,13 +1,19 @@
 import mongoose from "mongoose";
 
-const uri = process.env.MONGODB_URI || "";
+const isProduction = process.env.NODE_ENV === "production";
+
+const uri = isProduction
+  ? process.env.MONGODB_URI || ""
+  : process.env.MONGODB_URI_DEV || "";
 
 export async function connectToDatabase() {
   if (mongoose.connection.readyState >= 1) return;
 
   try {
     await mongoose.connect(uri);
-    console.log("✅ Connected to MongoDB Atlas");
+    console.log(
+      `✅ Connected to MongoDB Atlas (${isProduction ? "PROD" : "DEV"})`
+    );
   } catch (err) {
     console.error("❌ MongoDB connection error:", err);
     throw err;
